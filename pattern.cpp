53358f24386f07262235a2858061e9613348d74c
KashishBansal7 <kashishbansal002@gmail.com>
2025-09-18 18:18:02 +0530

    tut11.cpp

---
+++ pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     for(int i=1;i<5;i++){
+//         for(int j=1;j<=5;j++){
+//         cout<<" * ";
+//     }
+//     cout<<endl;
+// }
+
+// }
+// question 2
+//     int i,j;
+//      for(i=1;i<=5;i++){
+//         for(j=1;j<=5;j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//      }
+// }
+// question 3
+
+//      for(int i=1;i<=5;i++){
+//         for(int j=5;j>=1;j--){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+    
+// }
+// question 4
+
+     
+}
+
---
+++ tut11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num[]={22,84,94,82,40,24,54,22};
+    cout<<num[0]<<endl;
+    cout<<num[1]<<endl;
+    cout<<num[2]<<endl;
+    cout<<num[3]<<endl;
+    cout<<num[4]<<endl;
+    cout<<num[5]<<endl;
+    // int *p=num;
+    // cout<<*p<<endl;
+    // cout<<*(p+1)<<endl;
+    // cout<<*(p+2)<<endl;
+    // cout<<*(p+3)<<endl;
+    //  cout<<*(p+4)<<endl;
+
+}
+  
