0a23efe86dcc40f7a55837267a0a629705f21a24
KashishBansal7 <kashishbansal002@gmail.com>
2025-10-06 23:32:02 +0530

    funtion & function prototype

---
+++ tut15.cpp
@@ -0,0 +1,42 @@
+                                //function & function prototype
+
+#include<iostream>
+using namespace std;
+// int sum( int a, int b){
+//     int c=a+b;
+//     return c;
+// }
+
+// int main(){                    //int main() is main function
+//     int num1,num2;
+//     cout<<"enter first number"<<endl;
+//     cin>>num1;
+//     cout<<"enter second number"<<endl;
+//     cin>>num2;
+//     cout<<"the sun is "<<sum(num1,num2);
+
+//     return 0;
+// }
+
+
+                                       //function prototype 
+
+//type funtion (argument)
+//   int sum (int a,int b);
+//   int main(){                    //int main() is main function
+//       int num1,num2;              //num1 and num2 are actual  perameter
+//       cout<<"enter first number"<<endl;
+//       cin>>num1;
+//       cout<<"enter second number"<<endl;
+//       cin>>num2;
+//       cout<<"the sun is "<<sum(num1,num2);
+
+//       return 0;
+// }
+
+// int sum( int a, int b){          // here a and b is formal perameter
+//     int c=a+b;
+//     return c;
+// }
+
+
